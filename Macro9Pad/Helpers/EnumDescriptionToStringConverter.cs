da2f41cac1c61859c12f18a63bd2e4556fbaa5b6
Zachery Littell <zlittell@gmail.com>
2020-11-24 13:58:20 -0600

    documentation

--- Macro9Pad/Helpers/EnumDescriptionToStringConverter.cs
+++ Macro9Pad/Helpers/EnumDescriptionToStringConverter.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// <copyright file="EnumDescriptionToStringConverter.cs" company="Mechanical Squid Factory">
+// Copyright © Mechanical Squid Factory Licensed under the Unlicense.
+// </copyright>
+
+using System;
 using System.Globalization;
-using System.Text;
 using System.Windows.Data;
 
 namespace Macro9Pad.Helpers
 {
+  /// <summary>Converts an Enum to a string of its descriptor.</summary>
   public class EnumDescriptionToStringConverter : IValueConverter
   {
+    /// <inheritdoc/>
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
       if (value != null)
@@ -18,6 +22,7 @@ namespace Macro9Pad.Helpers
       return null;
     }
 
+    /// <inheritdoc/>
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
       throw new NotImplementedException();
